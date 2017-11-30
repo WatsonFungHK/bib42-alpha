cbccd0c6815cb784beac06f28a9d6b2b97131435
WatsonFungHK <watson.cwfung@gmail.com>
2017-11-30 12:26:59 +0800

    front: modify filterpipe function

--- frontend/src/app/filter.pipe.ts
+++ frontend/src/app/filter.pipe.ts
@@ -4,13 +4,14 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'filter'
 })
 export class FilterPipe implements PipeTransform {
-// hahaha
+
   transform(races: any, term: any): any {
+    term = term.toLowerCase()
     if ( term == '') return races
     return races.filter( race => {
-      return  race.race_name.toLowerCase().includes(term.toLowerCase()) ||
-              race.city.toLowerCase().includes(term.toLowerCase()) ||
-              race.country.toLowerCase().includes(term.toLowerCase())
+      return  race.race_name.toLowerCase().includes(term) ||
+              race.city.toLowerCase().includes(term) ||
+              race.country.toLowerCase().includes(term)
     })
   }
 
