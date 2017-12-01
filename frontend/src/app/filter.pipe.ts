db7cd36aa9ef2a8686ed310c4c8cec5fd7e3211a
WatsonFungHK <watson.cwfung@gmail.com>
2017-12-01 11:47:31 +0800

    front: fixed month name filter function

--- frontend/src/app/filter.pipe.ts
+++ frontend/src/app/filter.pipe.ts
@@ -4,26 +4,34 @@ import { Pipe, PipeTransform } from "@angular/core";
   name: "filter"
 })
 export class FilterPipe implements PipeTransform {
-  month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
   transform(races: any[], term: string): any {
+  const month = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"];
     term = term.toLowerCase();
     if ( term === "") {
       return races;
     }
     return races.filter( race => {
-      // // tslint:disable-next-line:radix
-      // const monthIdx = parseInt(race.race_date.split("-")[1]);
-      // console.log(monthIdx + typeof(monthIdx));
-      // const monthName = this.month[monthIdx];
-      // const monthName: string = race.race_date.getLongMonth();
-      // console.log(monthName);
+      // tslint:disable-next-line:radix
+      const monthIdx = parseInt(race.race_date.split("-")[1]) - 1;
+      const monthString = month[monthIdx];
+      console.log(`${race.race_name} ${race.race_date}: ${monthString} ${monthIdx}`);
       return  race.race_name.toLowerCase().includes(term) ||
               race.city.toLowerCase().includes(term) ||
-              race.country.toLowerCase().includes(term)
-              ;
-              // ||
-              // monthName.includes(term);
+              race.country.toLowerCase().includes(term) ||
+              monthString.toLowerCase().includes(term);
     });
   }
 
